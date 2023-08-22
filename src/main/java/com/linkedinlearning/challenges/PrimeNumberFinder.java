5510be22151618db77cb53b3fd86af00ada3bad0
BoGlavatki <82083692+BoGlavatki@users.noreply.github.com>
2023-08-22 12:42:32 +0000

    prime 2

--- src/main/java/com/linkedinlearning/challenges/PrimeNumberFinder.java
+++ src/main/java/com/linkedinlearning/challenges/PrimeNumberFinder.java
@@ -4,11 +4,11 @@ public class PrimeNumberFinder {
   static List<Integer> findPrimeNumbersInInterval(int a, int b) {
     List<Integer> primeNumbers = new ArrayList<Integer>();
     for (int i = a; i < b + 1; i++) {
-      if (!(i == 0) && !(i == 3) && !(i == 5) && !(i == 7) && !(i == 1) && !(i % 2 == 0) && !(i % 3 == 0)
+      if (!(i == 2) && !(i == 0) && !(i == 3) && !(i == 5) && !(i == 7) && !(i == 1) && !(i % 2 == 0) && !(i % 3 == 0)
           && !(i % 4 == 0) && !(i % 5 == 0) && !(i % 6 == 0) && !(i % 7 == 0) && !(i % 8 == 0) && !(i % 9 == 0)) {
         System.out.println("i ist primzahl: " + i);
         primeNumbers.add(i);
-      } else if ((i == 3) || (i == 5) || (i == 7)) {
+      } else if ((i == 2) || (i == 3) || (i == 5) || (i == 7)) {
         System.out.println("i ist primzahl: " + i);
         primeNumbers.add(i);
       } else if ((i == 0) || (i == 1)) {
